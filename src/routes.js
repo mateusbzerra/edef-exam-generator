d01d8c78550d452f2246aa140265393cbafc2768
Mateus Bezerra <mateusbzerra@gmail.com>
2019-10-15 13:59:13 -0300

    feat: AdminMiddleware added

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const AuthController = require('./controllers/AuthController');
 const UserController = require('./controllers/UserController');
 const DisciplineController = require('./controllers/DisciplineController');
 const AuthMiddleware = require('./middlewares/AuthMIddleware');
+const AdminMiddleware = require('./middlewares/AdminMiddleware');
 
 const routes = express.Router();
 
@@ -17,8 +18,8 @@ routes.use(AuthMiddleware);
 
 routes.get('/disciplines', DisciplineController.index);
 routes.get('/disciplines/:id', DisciplineController.show);
-routes.post('/disciplines', DisciplineController.store);
-routes.put('/disciplines/:id', DisciplineController.update);
-routes.delete('/disciplines/:id', DisciplineController.delete);
+routes.post('/disciplines', AdminMiddleware, DisciplineController.store);
+routes.put('/disciplines/:id', AdminMiddleware, DisciplineController.update);
+routes.delete('/disciplines/:id', AdminMiddleware, DisciplineController.delete);
 
 module.exports = routes;
