1f743caa13099c6b1dd5f3120a4684b23a7b8067
Mateus Bezerra <mateusbzerra@gmail.com>
2019-10-15 10:55:23 -0300

    feat:Auth middleware added

--- src/middlewares/AuthMIddleware.js
+++ src/middlewares/AuthMIddleware.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   if (req.headers.authorization) {
     const header = req.headers.authorization;
+
     const [aux, token] = header.split(' ');
 
     try {
@@ -12,5 +13,7 @@ module.exports = (req, res, next) => {
     } catch (err) {
       return res.status(401).json({ error: 'Token invalid' });
     }
+  } else {
+    return res.status(401).json({ error: 'No Token was provided' });
   }
 };
