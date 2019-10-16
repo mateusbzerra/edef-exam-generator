0636b110cf9769567898b61c1cd2455016c0503a
Mateus Bezerra <mateusbzerra@gmail.com>
2019-10-15 23:44:10 -0300

    feat: List, show and remove users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,11 +2,55 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
 class UserController {
+  async index(req, res) {
+    try {
+      const users = await User.find({});
+      return res.json(users);
+    } catch (err) {
+      return res.status(400).json({ error: err });
+    }
+  }
+  async show(req, res) {
+    const reqUser = await User.findById(req.user.id);
+    if (reqUser.admin || req.user.id === req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        return res.json(user);
+      } catch (err) {
+        return res.status(400).json({ error: err });
+      }
+    } else {
+      return res.status(401).json({ error: 'Not Allowed' });
+    }
+  }
   async store(req, res) {
     req.body.password = await bcrypt.hash(req.body.password, 8);
     try {
       const user = await User.create(req.body);
       return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: err });
+    }
+  }
+  async update(req, res) {
+    const reqUser = await User.findById(req.user.id);
+    if (reqUser.admin || req.user.id === req.params.id) {
+      try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+          new: true
+        });
+        return res.json(user);
+      } catch (err) {
+        return res.status(400).json({ error: err });
+      }
+    } else {
+      return res.status(401).json({ error: 'Not Allowed' });
+    }
+  }
+  async delete(req, res) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      return res.json({ success: 'User deleted successfully' });
     } catch (err) {
       return res.status(400).json({ error: err });
     }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AuthController = require('./controllers/AuthController');
 const UserController = require('./controllers/UserController');
 const DisciplineController = require('./controllers/DisciplineController');
+//
 const AuthMiddleware = require('./middlewares/AuthMIddleware');
 const AdminMiddleware = require('./middlewares/AdminMiddleware');
 
@@ -15,11 +16,18 @@ routes.post('/login', AuthController.auth);
 routes.post('/signup', UserController.store);
 
 routes.use(AuthMiddleware);
-
+//DISCIPLINES
 routes.get('/disciplines', DisciplineController.index);
 routes.get('/disciplines/:id', DisciplineController.show);
 routes.post('/disciplines', AdminMiddleware, DisciplineController.store);
 routes.put('/disciplines/:id', AdminMiddleware, DisciplineController.update);
 routes.delete('/disciplines/:id', AdminMiddleware, DisciplineController.delete);
+
+//USER
+routes.get('/users', AdminMiddleware, UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', AdminMiddleware, UserController.store);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', AdminMiddleware, UserController.delete);
 
 module.exports = routes;
